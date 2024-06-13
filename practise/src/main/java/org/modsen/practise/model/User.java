f03adf014656f0c67ee71d9e20ea86aa9ae58d9f
Nikita Krashevskiy <nikita.krashevskiy@gmail.com>
2024-06-13 17:13:07 +0300

    feat: add user model

---
+++ practise/src/main/java/org/modsen/practise/model/User.java
@@ -0,0 +1,60 @@
+package org.modsen.practise.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(unique = true)
+    private String login;
+
+    private String password;
+
+    public User() {
+    }
+
+    public User(String email, String login, String password) {
+        this.email = email;
+        this.login = login;
+        this.password = password;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
