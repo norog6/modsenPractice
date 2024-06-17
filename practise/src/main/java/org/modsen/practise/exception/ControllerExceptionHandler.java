3b1fa7a191972c291413e1791eba41e310751c85
loziuk <loziukvika10@gmail.com>
2024-06-17 07:26:35 +0300

    feat: validation exception handling

--- practise/src/main/java/org/modsen/practise/exception/ControllerExceptionHandler.java
+++ practise/src/main/java/org/modsen/practise/exception/ControllerExceptionHandler.java
@@ -1,15 +1,45 @@
 package org.modsen.practise.exception;
-
+import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import javax.validation.ConstraintViolationException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
     @ExceptionHandler(CustomException.class)
-    public ResponseEntity<ErrorMessage> customException(CustomException ex){
+    public ResponseEntity<ErrorMessage> onCustomException(CustomException ex){
         ErrorMessage errorMessage = new ErrorMessage(ex.getStatus(), ex.getMessage());
         return new ResponseEntity<>(errorMessage, HttpStatusCode.valueOf(ex.getStatus()));
+    }
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ValidationErrorMessage> onConstraintValidationException(
+            ConstraintViolationException ex) {
+        final List<Violation> violations = ex.getConstraintViolations().stream()
+                .map(
+                        violation -> new Violation(
+                                violation.getPropertyPath().toString(),
+                                violation.getMessage()
+                        )
+                )
+                .collect(Collectors.toList());
+        ValidationErrorMessage errorMessage = new ValidationErrorMessage(violations);
+
+        return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationErrorMessage> onMethodArgumentNotValidException(
+            MethodArgumentNotValidException e) {
+        final List<Violation> violations = e.getBindingResult().getFieldErrors().stream()
+                .map(error -> new Violation(error.getField(), error.getDefaultMessage()))
+                .collect(Collectors.toList());
+        ValidationErrorMessage errorMessage = new ValidationErrorMessage(violations);
+
+        return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
     }
 }
---
+++ practise/src/main/java/org/modsen/practise/exception/ValidationErrorMessage.java
@@ -0,0 +1,11 @@
+package org.modsen.practise.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+@Getter
+@RequiredArgsConstructor
+public class ValidationErrorMessage {
+    private final List<Violation> violations;
+}
