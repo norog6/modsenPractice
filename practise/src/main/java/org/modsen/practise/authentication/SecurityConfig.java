a5f6cd05768d536b280d59635796c731baa9f4d3
Nikita Krashevskiy <nikita.krashevskiy@gmail.com>
2024-06-18 12:04:35 +0300

    style: add tab indent between package and import lines

--- practise/src/main/java/org/modsen/practise/authentication/SecurityConfig.java
+++ practise/src/main/java/org/modsen/practise/authentication/SecurityConfig.java
@@ -1,4 +1,5 @@
 package org.modsen.practise.authentication;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
