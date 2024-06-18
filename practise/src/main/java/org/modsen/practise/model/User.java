37857d33e79ef9c9321525ef9b8c3beabe6f4873
Nikita Krashevskiy <nikita.krashevskiy@gmail.com>
2024-06-18 11:51:03 +0300

    fix: add lombok annotations

--- practise/src/main/java/org/modsen/practise/model/User.java
+++ practise/src/main/java/org/modsen/practise/model/User.java
@@ -1,8 +1,16 @@
 package org.modsen.practise.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 @Table(name = "users")
 public class User {
     @Id
@@ -17,44 +25,4 @@ public class User {
 
     private String password;
 
-    public User() {
-    }
-
-    public User(String email, String username, String password) {
-        this.email = email;
-        this.username = username;
-        this.password = password;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String login) {
-        this.username = login;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
