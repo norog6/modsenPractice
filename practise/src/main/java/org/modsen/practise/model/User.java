44e541d8310f20700ce7adebf6e8d84d3c8ecc50
loziuk <loziukvika10@gmail.com>
2024-06-18 12:22:41 +0300

    fix: add user model

---
+++ practise/src/main/java/org/modsen/practise/model/User.java
@@ -0,0 +1,27 @@
+package org.modsen.practise.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "email", unique = true)
+    private String email;
+
+    @Column(name = "login",unique = true)
+    private String login;
+
+    @Column(name = "password")
+    private String password;
+}
