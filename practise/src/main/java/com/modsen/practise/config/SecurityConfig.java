26f25b9a183093cacc7063a5005fa4ee747a6d09
Nikita Krashevskiy <nikita.krashevskiy@gmail.com>
2024-06-24 15:20:03 +0300

    feat: add logout

--- practise/src/main/java/com/modsen/practise/config/SecurityConfig.java
+++ practise/src/main/java/com/modsen/practise/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.modsen.practise.config;
 
 import com.modsen.practise.jwt.JwtFilter;
+import com.modsen.practise.jwt.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +9,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -17,6 +19,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig {
 
     private final JwtFilter jwtFilter;
+    private final AuthService authService;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -26,8 +29,17 @@ public class SecurityConfig {
                 .authorizeHttpRequests(
                         request -> request
                                 .requestMatchers("/api/auth/login", "/api/auth/token", "/api/auth/registration")
-                                    .permitAll()
+                                .permitAll()
                                 .anyRequest().authenticated()
+                )
+                .logout(
+                        logout -> logout
+                                .logoutUrl("/api/auth/logout")
+                                .addLogoutHandler(authService)
+                                .logoutSuccessHandler(
+                                        ((request, response, authentication) ->
+                                                SecurityContextHolder.clearContext())
+                                )
                 )
                 .addFilterAfter(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
