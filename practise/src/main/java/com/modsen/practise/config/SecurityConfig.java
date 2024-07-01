250aa1143062f93e86481b9c6681e5b9c71c6de9
norog6 <92385720+norog6@users.noreply.github.com>
2024-07-01 10:11:01 +0300

    split dto to request and response, fix mappers

--- practise/src/main/java/com/modsen/practise/config/SecurityConfig.java
+++ practise/src/main/java/com/modsen/practise/config/SecurityConfig.java
@@ -25,11 +25,9 @@ public class SecurityConfig {
                 .sessionManagement(manager -> manager.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(
                         request -> request
-                                .requestMatchers("/api/auth/login", "/api/auth/token", "/api/auth/registration","/api/categories"
-                                ,"/api/products","/api/users","/api/order-items",
-                                        "/api/orders")
+                                .requestMatchers("/api/auth/login", "/api/auth/token", "/api/auth/registration")
                                     .permitAll()
-                                .anyRequest().permitAll()
+                                .anyRequest().authenticated()
                 )
                 .addFilterAfter(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
